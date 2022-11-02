96d164ee5c3c1638a14fde2bb7c2cc9b09be583d
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2022-11-03 00:30:08 +0300

    V1.0.3

--- plugin.rb
+++ plugin.rb
@@ -10,17 +10,14 @@
 enabled_site_setting :plugin_name_enabled
 
 after_initialize do
-  class ::Topic
-    after_update :update_tags
-
-    def update_topic_tags
-      # check if topic has tag needs support and either closed or visible has changed
-      needs_support_tag = Tag.find_by(name: "Needs-Support")
-
-      if needs_support_tag && self.tags.include?(needs_support_tag)
-        if self.visible == false || self.closed == true
-          self.tags.delete needs_support_tag
-        end
+  DiscourseEvent.on(:topic_status_updated) do |topic, status, enabled|
+    needs_support_tag = Tag.find_by(name: "Needs-Support")
+    if needs_support_tag && topic.tags.include?(needs_support_tag)
+      case status
+      when "closed"
+        topic.tags.delete needs_support_tag if topic.closed == true
+      when "visible"
+        topic.tags.delete needs_support_tag if topic.visible == false
       end
     end
   end
